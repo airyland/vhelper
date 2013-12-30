978dbafe429091d967ebab3cd8bc8dcefdbed0e6
李政 <airyland@qq.com>
2013-12-30 18:12:42 +0800

    add getWeekData function

--- src/date.js
+++ src/date.js
@@ -1,4 +1,18 @@
+// get week 
 Date.prototype.getWeek = function() {
   var onejan = new Date(this.getFullYear(),0,1);
   return Math.ceil((((this - onejan) / 86400000) + onejan.getDay()+1)/7);
+}
+
+// get the start and end date of curretn week
+
+var getWeekDate = function(){
+  var curr = new Date; // get current date
+  var first = curr.getDate() - curr.getDay(); // First day is the day of the month - the day of the week
+  var last = first + 6; // last day is the first day + 6
+
+  var firstday = new Date(curr.setDate(first)).toUTCString();
+  var lastday = new Date(curr.setDate(last)).toUTCString();
+  
+  return [firstday,lastday];
 }
