93da23ec7ee4181d5fb59fa03a79a008a9b81683
李政 <airyland@qq.com>
2013-12-30 18:15:48 +0800

    update date.js

--- src/date.js
+++ src/date.js
@@ -16,3 +16,30 @@ var getWeekDate = function(){
   
   return [firstday,lastday];
 }
+
+// get dates between two dates
+
+   Date.prototype.addDays = function(days) {
+       var dat = new Date(this.valueOf())
+       dat.setDate(dat.getDate() + days);
+       return dat;
+   }
+
+ function getDates(startDate, stopDate) {
+    var dateArray = new Array();
+    var currentDate = startDate;
+    while (currentDate <= stopDate) {
+      dateArray.push(currentDate)
+      currentDate = currentDate.addDays(1);
+    }
+    return dateArray;
+  }
+
+var dateArray = getDates(new Date(), (new Date()).addDays(10));
+for (i = 0; i < dateArray.length; i ++ ) {
+    console.log(dateArray[i]);
+}
+
+var getDatesRange = function(){
+  // not finished yet
+}
